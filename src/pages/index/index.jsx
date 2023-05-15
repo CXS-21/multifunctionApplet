97f960975fbee8aaa0f05401cb0d202dee158b5e
erick <chengxiansheng_21@163.com>
2023-05-15 17:16:05 +0800

    首页增加匹配输入框

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,147 +1,169 @@
-import { Component } from "react";
-import { View, Text, Button, Image } from "@tarojs/components";
+import { useState } from "react";
+import { View, Text, Button, Image, Input } from "@tarojs/components";
 import Taro from "@tarojs/taro";
-import { connect } from "react-redux";
+import { useDebounceFn } from "ahooks";
 import "./index.less";
-@connect(({ user }) => ({
-  userInfo: user.userInfo,
-}))
-export default class Index extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      list: [
-        {
-          id: 1,
-          info: "资源预约",
-          url: "/pages/appointment/index",
-        },
-        {
-          id: 2,
-          info: "一键切换中英文",
-          url: "/pages/changeLanguage/index",
-        },
-        {
-          id: 3,
-          info: "自定义顶部导航栏",
-          url: "/pages/navigationBar/index",
-        },
-        {
-          id: 4,
-          info: "周历",
-          url: "/pages/week/index",
-        },
-        {
-          id: 5,
-          info: "左滑删除",
-          url: "/pages/moveLeftDel/index",
-        },
-        {
-          id: 6,
-          info: "列表局部加载",
-          url: "",
-        },
-        {
-          id: 7,
-          info: "视频播放",
-          url: "/pages/video/index",
-        },
-        {
-          id: 8,
-          info: "图片压缩",
-          url: "/pages/img/index",
-        },
-        {
-          id: 9,
-          info: "操作菜单",
-          url: "/pages/menu/index",
-        },
-        {
-          id: 10,
-          info: "交互回调效果",
-          url: "/pages/interaction/index",
-        },
-        {
-          id: 11,
-          info: "图表集",
-          url: "/pages/chart/index",
-        },
-        {
-          id: 12,
-          info: "------",
-          url: "",
-        },
-        {
-          id: 13,
-          info: "------",
-          url: "",
-        },
-        {
-          id: 14,
-          info: "------",
-          url: "",
-        },
-        {
-          id: 15,
-          info: "------",
-          url: "",
-        },
-        {
-          id: 16,
-          info: "------",
-          url: "",
-        },
-      ],
-    };
-  }
+const dataList = [
+  {
+    id: 1,
+    label: "资源预约",
+    url: "/pages/appointment/index",
+  },
+  {
+    id: 2,
+    label: "一键切换中英文",
+    url: "/pages/changeLanguage/index",
+  },
+  {
+    id: 3,
+    label: "自定义顶部导航栏",
+    url: "/pages/navigationBar/index",
+  },
+  {
+    id: 4,
+    label: "周历",
+    url: "/pages/week/index",
+  },
+  {
+    id: 5,
+    label: "左滑删除",
+    url: "/pages/moveLeftDel/index",
+  },
+  {
+    id: 6,
+    label: "列表局部加载",
+    url: "",
+  },
+  {
+    id: 7,
+    label: "视频播放",
+    url: "/pages/video/index",
+  },
+  {
+    id: 8,
+    label: "图片压缩",
+    url: "/pages/img/index",
+  },
+  {
+    id: 9,
+    label: "操作菜单",
+    url: "/pages/menu/index",
+  },
+  {
+    id: 10,
+    label: "交互回调效果",
+    url: "/pages/interaction/index",
+  },
+  {
+    id: 11,
+    label: "图表集",
+    url: "/pages/chart/index",
+  },
+  {
+    id: 12,
+    label: "------",
+    url: "",
+  },
+  {
+    id: 13,
+    label: "------",
+    url: "",
+  },
+  {
+    id: 14,
+    label: "------",
+    url: "",
+  },
+  {
+    id: 15,
+    label: "------",
+    url: "",
+  },
+  {
+    id: 16,
+    label: "------",
+    url: "",
+  },
+];
+export default () => {
+  const [menuList, setMenuList] = useState(dataList);
+  const [search, setSearch] = useState("");
 
-  componentWillMount() {}
+  const { run: handleSearch } = useDebounceFn(
+    async (e) => {
+      onMatch(e);
+    },
+    {
+      wait: 500,
+    }
+  );
 
-  componentDidMount() {}
+  const onMatch = (value) => {
+    let total;
+    if (value) {
+      let newList = [];
+      menuList.map((item) => {
+        if (item.label.includes(value)) newList.push(item);
+      });
+      setMenuList(newList);
+      total = newList.length;
+    } else {
+      setMenuList(dataList);
+      total = dataList.length;
+    }
+    Taro.showToast({
+      title: `共找到${total}条数据`,
+      icon: "none",
+      duration: 2000,
+    });
+  };
 
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-  handleNav = (item) => {
+  const handleNav = (item) => {
     Taro.navigateTo({
       url: item.url,
     });
   };
-  render() {
-    const { list } = this.state;
-    return (
-      <View className="container">
-        <View className="content">
-          {list &&
-            list.length > 0 &&
-            list.map((item) => {
-              return (
-                <Button
-                  key={item.id}
-                  className={"item"}
-                  onClick={() => {
-                    this.handleNav(item);
-                  }}
-                >
-                  <Text>{item.info}</Text>
-                </Button>
-              );
-            })}
-          <View
-            className="back"
-            onClick={() => {
-              Taro.pageScrollTo({
-                scrollTop: 0,
-                duration: 200,
-              });
-            }}
-          >
-            <Image src={require("../../assets/back.png")} className={"pic"} />
-          </View>
+
+  return (
+    <View className="index_container">
+      <View className="index_search_box">
+        <Input
+          onInput={(e) => {
+            handleSearch(e.target.value);
+            setSearch(e.target.value);
+          }}
+        />
+        <Image
+          src={require("../../assets/search.png")}
+          onClick={() => onMatch(search)}
+        />
+      </View>
+      <View className="index_content">
+        {menuList &&
+          menuList.length > 0 &&
+          menuList.map((item) => {
+            return (
+              <Button
+                key={item.id}
+                className={"item"}
+                onClick={() => handleNav(item)}
+              >
+                <Text>{item.label}</Text>
+              </Button>
+            );
+          })}
+        <View
+          className="back_top"
+          onClick={() => {
+            Taro.pageScrollTo({
+              scrollTop: 0,
+              duration: 200,
+            });
+          }}
+        >
+          <Image src={require("../../assets/back.png")} className={"pic"} />
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
